b9fcc06860ee55c03745cbef52cc8a98a9bb6d03
Bence Romsics <rubasov@gmail.com>
2013-03-17 14:56:23 +0100

    Add join().

--- bashrc.d/function.sh
+++ bashrc.d/function.sh
@@ -4,3 +4,26 @@ ckdir () {
     mkdir -p "$@"
     cd "$1"
 }
+
+# join 2nd and later arguments by 1st argument
+join () {
+
+    # Could be much simpler if it was enough for $1 to be a single char.
+    #
+    # local IFS="$1"
+    # shift
+    # builtin echo "$*"
+
+    local sep="$1"
+    shift
+
+    builtin echo -n $1
+    shift
+
+    for arg in "$@"
+    do
+        builtin echo -n "$sep$arg"
+    done
+
+    builtin echo
+}
