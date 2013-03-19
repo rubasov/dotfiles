2ffbcf3fb7758fbd58cf31edfb4b608e7e1a3d2e
Bence Romsics <rubasov@gmail.com>
2013-03-19 08:48:33 +0100

    Don't repeat yourself and use local variables.

--- bashrc.d/completion.sh
+++ bashrc.d/completion.sh
@@ -1,2 +1,8 @@
-test -f /etc/bash_completion &&
-    . /etc/bash_completion
+enable_completion () {
+    local completion_file="${1:-/etc/bash_completion}"
+    if [ -f "$completion_file" ]
+    then
+        . "$completion_file"
+    fi
+}
+enable_completion
