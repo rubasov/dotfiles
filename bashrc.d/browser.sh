1df69feb341b71b4937a215187f2a300e655099d
Bence Romsics <rubasov@gmail.com>
2013-03-19 08:47:49 +0100

    Use local variables.

--- bashrc.d/browser.sh
+++ bashrc.d/browser.sh
@@ -1,8 +1,12 @@
-for __browser in chromium firefox elinks links2 lynx
-do
-    if [ -x $( which $__browser ) ]
-    then
-        export BROWSER=$__browser
-        break
-    fi
-done
+export_preferred_browser () {
+    local browser
+    for browser in "$@"
+    do
+        if [ -x $( which "$browser" ) ]
+        then
+            export BROWSER="$browser"
+            break
+        fi
+    done
+}
+export_preferred_browser chromium firefox elinks links2 lynx
