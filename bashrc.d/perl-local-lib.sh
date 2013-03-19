126345fd6a7e2f46ebb88eabf254f3c8e2c7e5f4
Bence Romsics <rubasov@gmail.com>
2013-03-19 08:52:46 +0100

    Comment changes.

--- bashrc.d/perl-local-lib.sh
+++ bashrc.d/perl-local-lib.sh
@@ -1,3 +1,2 @@
-# PERL5LIB="$HOME/lib/perl${PERL5LIB:+":$PERL5LIB"}"
 # (cf) env -i perl -Mlocal::lib=$HOME
-eval $( perl -Mlocal::lib=$HOME | grep -vw PATH )
+eval $( perl -Mlocal::lib="$HOME" | grep -vw PATH )
