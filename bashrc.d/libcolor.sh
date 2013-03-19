8a58e3b2312fd43c53e6e4189d162d18715ffe4f
Bence Romsics <rubasov@gmail.com>
2013-03-19 09:01:03 +0100

    Cache colors.

--- bashrc.d/libcolor.sh
+++ bashrc.d/libcolor.sh
@@ -1,7 +1,7 @@
 # avoid hardcoding ANSI color codes
-__reset=$( tput sgr0 )
-__bold=$( tput bold )
-__red=$( tput setaf 1 )
-__green=$( tput setaf 2 )
-__yellow=$( tput setaf 3 )
-__purple=$( tput setaf 5 )
+__reset=${__reset:-$( tput sgr0 )}
+__bold=${__bold:-$( tput bold )}
+__red=${__red:-$( tput setaf 1 )}
+__green=${__green:-$( tput setaf 2 )}
+__yellow=${__yellow:-$( tput setaf 3 )}
+__purple=${__purple:-$( tput setaf 5 )}
